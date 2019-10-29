d7cb951fd461ebd713e97a89f6f9795d69f2f640
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2019-10-29 18:42:05 +0100

    Update razzle.config.js

--- razzle.config.js
+++ razzle.config.js
@@ -6,39 +6,8 @@ module.exports = {
 
     if (target === 'web' && dev) {
       config.devServer.quiet = false;
-      config.devServer.public = `${process.env.RAZZLE_PUBLIC_HOST}:${process.env.RAZZLE_PUBLIC_PORT}`;
-
-      config.module.rules = config.module.rules.reduce((rules, rule) => {
-        if (rule.test &&
-          rule.test.toString()===/\.(js|jsx|mjs)$/.toString() &&
-          !rule.enforce) {
-
-          const { use, ...rest } = rule;
-
-          rules.push({ ...rule, ...{
-            exclude: /razzle-dev-utils\/webpackHotDevClient\.js/
-          }});
-
-          rules.push({ ...rest, ...{
-            use: [ ...use, {
-              loader: require.resolve('string-replace-loader'),
-              options: {
-                search: 'port: parseInt(process.env.PORT || window.location.port, 10) + 1,'
-                  .replace(/[-[\]{}()*+!<=:?.\/\\^$|#\s,]/g, '\\$&'),
-                replace: "port: '',",
-                flags: 'g',
-                strict: true
-              }
-            }],
-            include: /razzle-dev-utils\/webpackHotDevClient\.js/,
-          }});
-        }
-        else {
-          rules.push(rule);
-        } 
-        return rules;
-      }, []);
     }
     return config;
-  }
+  },
+  plugins: [ 'proxy' ]
 };
